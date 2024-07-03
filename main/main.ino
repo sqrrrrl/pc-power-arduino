3999afea11dc57a9e32fd733cd7b398135ce5bd7
superbobby2000 <coudercleo10@gmail.com>
2024-07-02 20:15:04 -0400

    automatically send the pc power state when it changes

--- main/main.ino
+++ main/main.ino
@@ -1,19 +1,30 @@
 #include <ArduinoHttpClient.h>
+#include <Arduino_JSON.h>
 #include "WiFiS3.h"
 #include "environment.h"
 
 const int POWER_CONTROL_PIN = 3;
 const int RESET_CONTROL_PIN = 9;
-const int LED_0 = 6; // LED-
-const int LED_1 = 5; // LED+
+const uint8_t LED = A4; // LED+ Analog pin
 const int PRESS_TIME = 400;
 const int HARD_SHUTDOWN_PRESS_TIME = 5000;
+const int RETRY_TIMEOUT = 1000;
+const int ANALOG_PC_ON = 600;
+const int PC_STATUS_ON = 1;
+const int PC_STATUS_OFF = 0;
 
 const char ssid[] = SECRET_SSID;
 const char pass[] = SECRET_PASS;
+const char serverAddress[] = API_ADDRESS;
+const int serverPort = API_PORT;
+const String deviceId = DEVICE_ID;
+const String deviceSecret = DEVICE_SECRET;
 
-WiFiClient client;
 int status = WL_IDLE_STATUS;
+int lastPcStatus = PC_STATUS_OFF;
+int retries = 0;
+WiFiClient wifi;
+WebSocketClient ws = WebSocketClient(wifi, serverAddress, serverPort);
 
 void setup() {
   Serial.begin(9600);
@@ -25,9 +36,7 @@ void setup() {
     Serial.print("Attempting to connect to ");
     Serial.println(ssid);
     status = WiFi.begin(ssid, pass);
-    
-    // wait 10 seconds for connection:
-    delay(10000);
+    delay(RETRY_TIMEOUT);
   }
 
   Serial.println("Connected!");
@@ -45,7 +54,34 @@ void resetControlPins(){
 }
 
 void loop() {
-  resetControlPins();
+  delay(retries * RETRY_TIMEOUT);
+  Serial.println("Attempting to connect to the server");
+  if(ws.begin("/devices/gateway?device_id=" + deviceId + "&secret=" + deviceSecret) == 0){
+    retries = 0;
+    Serial.println("Connected!");
+  }
+
+  while (ws.connected()) {
+    int pcStatus = getPcStatus();
+    if(lastPcStatus != pcStatus){
+      lastPcStatus = pcStatus;
+      ws.beginMessage(TYPE_TEXT);
+      JSONVar message;
+      message["status"] = pcStatus;
+      ws.print(JSON.stringify(message));
+      ws.endMessage();
+    }
+
+    int messageSize = ws.parseMessage();
+
+    if (messageSize > 0) {
+      Serial.print("Received a message: ");
+      Serial.println(ws.readString());
+    }
+    resetControlPins();
+    delay(1000);
+  }
+  retries += 1;
 }
 
 void pressPowerSwitch() {
@@ -64,4 +100,11 @@ void hardShutdown(){
   pinMode(POWER_CONTROL_PIN, OUTPUT);
   digitalWrite(POWER_CONTROL_PIN, LOW);
   delay(HARD_SHUTDOWN_PRESS_TIME);
+}
+
+int getPcStatus(){
+  if(analogRead(LED) >= ANALOG_PC_ON){
+    return PC_STATUS_ON;
+  }
+  return PC_STATUS_OFF;
 }
